e44f75f5fa6c88786b0892d1672e5f117fd49c5f
ub1n <bin951024@naver.com>
2020-10-28 19:33:58 +0900

    위치바꾸기 구현완료

--- app/src/main/java/kr/ac/smu/cs/sopt_study/SampleActivity.kt
+++ app/src/main/java/kr/ac/smu/cs/sopt_study/SampleActivity.kt
@@ -7,23 +7,24 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_sample.*
 
-class SampleActivity : AppCompatActivity() {
-    private lateinit var sampleAdapter:SampleAdapter
+class SampleActivity : AppCompatActivity()  {
+    private lateinit var sampleAdapter: SampleAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sample)
 
-        sampleAdapter=SampleAdapter(this)
+        sampleAdapter = SampleAdapter(this)
 
-        sample_rcv.adapter=sampleAdapter
-        sample_rcv.layoutManager=LinearLayoutManager(this)
-        sampleAdapter.data= mutableListOf(
-            SampleData("박건우","두산베어스",R.drawable.img1,"2020년 10월 18일","두산의 선수이다"),
-            SampleData("김광현","SK와이번스",R.drawable.img2,"2020년 10월 18일","SK의 선수이다"),
-            SampleData("권혁","한화이글스",R.drawable.img3,"2020년 10월 18일","한화의 선수이다"),
-            SampleData("이정후","키움히어로즈",R.drawable.img4,"2020년 10월 18일","키움의 선수이다"),
-            SampleData("양현종","기아타이거즈",R.drawable.img5,"2020년 10월 18일","기아의 선수이다"),
-            SampleData("심수창","한화이글스",R.drawable.img6,"2020년 10월 18일","한화의 선수이다"))
+        sample_rcv.adapter = sampleAdapter
+        sample_rcv.layoutManager = LinearLayoutManager(this)
+        sampleAdapter.data = mutableListOf(
+            SampleData("박건우", "두산베어스", R.drawable.img1, "2020년 10월 18일", "두산의 선수이다"),
+            SampleData("김광현", "SK와이번스", R.drawable.img2, "2020년 10월 18일", "SK의 선수이다"),
+            SampleData("권혁", "한화이글스", R.drawable.img3, "2020년 10월 18일", "한화의 선수이다"),
+            SampleData("이정후", "키움히어로즈", R.drawable.img4, "2020년 10월 18일", "키움의 선수이다"),
+            SampleData("양현종", "기아타이거즈", R.drawable.img5, "2020년 10월 18일", "기아의 선수이다"),
+            SampleData("심수창", "한화이글스", R.drawable.img6, "2020년 10월 18일", "한화의 선수이다")
+        )
 
         sampleAdapter.notifyDataSetChanged()
 
@@ -35,33 +36,34 @@ class SampleActivity : AppCompatActivity() {
                 viewHolder: RecyclerView.ViewHolder,
                 target: RecyclerView.ViewHolder
             ): Boolean {
-                val adapter=sample_rcv.adapter as SampleAdapter
-                adapter.onItemMoved(viewHolder!!.adapterPosition,target!!.adapterPosition)
-                //showToast("on Move")
+                val adapter = sample_rcv.adapter as SampleAdapter
+                adapter.onItemMoved(viewHolder!!.adapterPosition, target!!.adapterPosition)
+
                 return true
+            }
+
+            override fun getMovementFlags(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder
+            ): Int {
+                val dragFlags = ItemTouchHelper.DOWN or ItemTouchHelper.UP
+                val swipeFlags = ItemTouchHelper.START or ItemTouchHelper.END
+                return ItemTouchHelper.Callback.makeMovementFlags(dragFlags, swipeFlags)
             }
 
             override fun onSwiped(viewHolder: RecyclerView.ViewHolder, swipeDir: Int) {
                 // 삭제되는 아이템의 포지션을 가져온다
                 val position = viewHolder.adapterPosition
                 // 데이터의 해당 포지션을 삭제한다
-                //showToast("on remove " + mList.remove(position))
+
 
                 // 아답타에게 알린다
 
                 val adapter = sample_rcv.adapter as SampleAdapter
                 adapter.remove(position)
-                //mRecyclerView.adapter?.notifyItemRemoved(position)
-
-
-
-                //memoList.removeAt(position)
-                //var intent=Intent(applicationContext,MainActivity::class.java)
-                // startActivity(intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK))
 
 
             }
-
 
 
         }
@@ -69,13 +71,9 @@ class SampleActivity : AppCompatActivity() {
         itemTouchHelper.attachToRecyclerView(sample_rcv)
 
 
-        /* var checkBox:CheckBox=findViewById(R.id.checkBox)
-         delbutton.setOnClickListener { view->
-             if(checkBox.isChecked)
-                 delbutton.text="선택됨"
-         }*/
 
     }
+
 
 
 
